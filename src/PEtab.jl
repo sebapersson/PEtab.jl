cbf691d5b0a4f16087501fbab292dd909040b77c
sebapersson <sebastian.persson1@gmail.com>
2023-02-19 20:40:00 +0100

    Add compat info, remove packages not used, and move includes into PEtab.jl

--- src/PEtab.jl
+++ src/PEtab.jl
@@ -25,15 +25,55 @@ using RuntimeGeneratedFunctions
 
 RuntimeGeneratedFunctions.init(@__MODULE__)
 
+include("PEtab_structs.jl")
+
+include("Common.jl")
+
+# Files related to computing the cost (likelihood)
+include(joinpath("Compute_cost", "Compute_priors.jl"))
+include(joinpath("Compute_cost", "Compute_cost.jl"))
+include(joinpath("Compute_cost", "Compute_cost_zygote.jl"))
+
+# Files related to computing derivatives
+include(joinpath("Derivatives", "Hessian.jl"))
+include(joinpath("Derivatives", "Gradient.jl"))
+include(joinpath("Derivatives", "Adjoint_sensitivity_analysis.jl"))
+include(joinpath("Derivatives", "Forward_sensitivity_equations.jl"))
+include(joinpath("Derivatives", "Gauss_newton.jl"))
+include(joinpath("Derivatives", "Common.jl"))
+
+# Files related to solving the ODE-system
+include(joinpath("Solve_ODE", "Change_experimental_condition.jl"))
+include(joinpath("Solve_ODE", "Solve_ode_Zygote.jl"))
+include(joinpath("Solve_ODE", "Solve_ode_model.jl"))
+
+# Files related to distributed computing
+include(joinpath("Distributed", "Distributed.jl"))
+
+# Files related to processing PEtab files
+include(joinpath("Process_PEtab_files", "Common.jl"))
+include(joinpath("Process_PEtab_files", "Get_simulation_info.jl"))
+include(joinpath("Process_PEtab_files", "Get_parameter_indices.jl"))
+include(joinpath("Process_PEtab_files", "Process_measurements.jl"))
+include(joinpath("Process_PEtab_files", "Process_parameters.jl"))
+include(joinpath("Process_PEtab_files", "Process_callbacks.jl"))
+include(joinpath("Process_PEtab_files", "Observables", "Common.jl"))
+include(joinpath("Process_PEtab_files", "Observables", "Create_h_sigma_derivatives.jl"))
+include(joinpath("Process_PEtab_files", "Observables", "Create_u0_h_sigma.jl"))
+include(joinpath("Process_PEtab_files", "Read_PEtab_files.jl"))
+
+# For creating a PEtab ODE problem
+include(joinpath("Create_PEtab_ODEProblem.jl"))
+
+# Creating the PEtab model
 include("Create_PEtab_model.jl")
 
+# Importing SBML models 
 include(joinpath("SBML", "SBML_to_ModellingToolkit.jl"))
 include(joinpath("SBML", "Common.jl"))
 include(joinpath("SBML", "Process_functions.jl"))
 include(joinpath("SBML", "Process_rules.jl"))
 
-
 export PEtabModel, PEtabODEProblem, readPEtabModel, setUpPEtabODEProblem
-
 
 end
