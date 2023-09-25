76c00f7b73fad0eb86dba59dc5352adbfebca8e9
Torkel <torkel.loman@gmail.com>
2023-09-25 11:42:39 -0400

    add waterfall plots

--- ext/PlotsExtension/Plot_recipes.jl
+++ ext/PlotsExtension/Plot_recipes.jl
@@ -1,5 +1,6 @@
 # The possible types of plots avaiable for PEtabOptimisationResult.
 const plot_types = [:objective, :best_objective]
+const plot_types_ms = [:objective, :best_objective, :waterfall]
 
 # Plots the objective function progression for a PEtabOptimisationResult.
 @recipe function f(res::PEtabOptimisationResult{T}; plot_type=:best_objective) where T
@@ -9,24 +10,32 @@ const plot_types = [:objective, :best_objective]
     end
 
     # Checks that a recognised plot type was used.
-    if !in(plot_type, [:objective, :best_objective])
+    if !in(plot_type, plot_types)
         error("Argument plot_type have an unrecognised value ($(plot_type)). Alternative values are: $(plot_types).")
     end
 
-    # Sets default values.
+    # Sets default values common for all plot types.
     xlabel --> "Function evaluations"
     yguide --> "Best objective value"
     yaxis --> :log10
-    seriestype --> (plot_type==:objective ? :scatter : :path)
      
-    # Finds the objective function values to plot.
-    best_vals = deepcopy(res.ftrace)
-    if plot_type == :best_objective
+    # Options for different plot types.
+    if plot_type == :objective
+        seriestype --> :scatter
+
+        x_vals = 1:(res.n_iterations+1)
+        y_vals = deepcopy(res.ftrace)
+    elseif plot_type == :best_objective
+        seriestype --> :path
+
+        x_vals = 1:(res.n_iterations+1)
+        y_vals = deepcopy(res.ftrace)
         for idx in 2:(res.n_iterations+1)
-            best_vals[idx] = min(res.ftrace[idx], best_vals[idx-1])
+            y_vals[idx] = min(res.ftrace[idx], y_vals[idx-1])
         end
     end
-    1:(res.n_iterations+1), best_vals
+
+    x_vals, y_vals
 end
 
 # Plots the objective function progressions for a PEtabMultistartOptimisationResult.
@@ -37,22 +46,38 @@ end
     end
 
     # Checks that a recognised plot type was used.
-    if !in(plot_type, [:objective, :best_objective])
-        error("Argument plot_type have an unrecognised value ($(plot_type)). Alternative values are: $(plot_types).")
+    if !in(plot_type, plot_types_ms)
+        error("Argument plot_type have an unrecognised value ($(plot_type)). Alternative values are: $(plot_types_ms).")
     end
 
-    # Sets default values.
-    xlabel --> "Function evaluations"
+    # Sets default values common for all plot types.
     yguide --> "Best objective value"
     yaxis --> :log10
-    seriestype --> (plot_type==:objective ? :scatter : :path)
-     
-    # Finds the objective function values to plot.
-    best_valss_ms = getfield.(res_ms.runs, :ftrace)
-    if plot_type == :best_objective
+
+    # Options for different plot types.
+    if plot_type == :objective
+        xlabel --> "Function evaluations"
+        seriestype --> :scatter
+
+        y_vals = getfield.(res_ms.runs, :ftrace)
+        x_vals = [1:length(best_vals) for best_vals in y_vals]
+    elseif plot_type == :best_objective
+        xlabel --> "Function evaluations"
+        seriestype --> :path
+
+        y_vals = getfield.(res_ms.runs, :ftrace)
         for (run_idx,run) in enumerate(res_ms.runs), idx in 2:(run.n_iterations+1)
-            best_valss_ms[run_idx][idx] = min(run.ftrace[idx], best_valss_ms[run_idx][idx-1])
+            y_vals[run_idx][idx] = min(run.ftrace[idx], y_vals[run_idx][idx-1])
         end
+        x_vals = [1:length(best_vals) for best_vals in y_vals]
+    elseif plot_type == :waterfall
+        label --> ""
+        seriestype --> :scatter
+        xlabel --> "Optimisation run"
+
+        y_vals = getfield.(res_ms.runs, :fmin)
+        x_vals = sortperm(sortperm(y_vals))
     end
-    [1:length(best_vals) for best_vals in best_valss_ms], best_valss_ms
+    
+    x_vals, y_vals
 end
