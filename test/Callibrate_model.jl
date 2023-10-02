6c7d6df5c136f3c899793808c2835f99e353f3c3
sebapersson <sebastian.persson1@gmail.com>
2023-10-02 15:15:36 +0200

    Allow multistart results to be read from disk

--- test/Callibrate_model.jl
+++ test/Callibrate_model.jl
@@ -34,8 +34,15 @@ using PEtab
     res2 = calibrate_model(petab_problem, p0, IpoptOptimiser(false), options=IpoptOptions(print_level=0))
     @test all(abs.(res2.xmin - petab_problem.θ_nominalT) .< 1e-2)
 
-    res3 = calibrate_model_multistart(petab_problem, Optim.IPNewton(), 10, nothing)
+    dir_save = joinpath(@__DIR__, "test_model_calibration")
+    res3 = calibrate_model_multistart(petab_problem, Optim.IPNewton(), 10, dir_save, save_trace=true)
+    res_read = PEtabMultistartOptimisationResult(dir_save)
     @test all(abs.(res3.xmin - petab_problem.θ_nominalT) .< 1e-2)
+    @test all(abs.(res_read.xmin - petab_problem.θ_nominalT) .< 1e-2)
+    @test res3.fmin == res_read.fmin
+    @test all(res3.runs[1].ftrace .== res_read.runs[1].ftrace)
+    @test all(res3.runs[1].xtrace .== res_read.runs[1].xtrace)
+    rm(dir_save, recursive=true)
 
     res4 = calibrate_model_multistart(petab_problem, IpoptOptimiser(true), 10, nothing, options=IpoptOptions(print_level=0))
     @test all(abs.(res4.xmin - petab_problem.θ_nominalT) .< 1e-2)
