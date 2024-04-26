849294f8f6cc2bb32ab5288906201d14bf82ffa6
sebapersson <sebastian.persson1@gmail.com>
2024-04-26 11:56:46 +0200

    Test startguesses via priors

--- test/Callibrate_model.jl
+++ test/Callibrate_model.jl
@@ -6,12 +6,13 @@ using Ipopt
 using PEtab
 using Optimization
 using OptimizationOptimJL
-
+using Catalyst
+using DataFrames
+using Distributions
 
 #=
     Test model callibration
 =#
-
 
 @testset "Test model callibration" begin
     # The Crauste model is numerically horrible, so here several start guesses will fail properly testing
@@ -22,7 +23,36 @@ using OptimizationOptimJL
     startguesses = generate_startguesses(petab_problem, 100; verbose=false,
                                          sampling_method=QuasiMonteCarlo.LatinHypercubeSample())
     @test startguesses isa Matrix{Float64}
-    @info "Done with start-guess test"
+    startguesses = generate_startguesses(petab_problem, 1; verbose=false,
+                                         sampling_method=QuasiMonteCarlo.LatinHypercubeSample())
+    @test startguesses isa Vector{Float64}
+    @info "Done with Crauste start-guess test"
+
+    # Test sample start-guesses when we have a prior that we sample from
+    rn = @reaction_network begin
+        @parameters a0 b0
+        @species A(t)=a0 B(t)=b0
+        (k1, k2), A <--> B
+    end
+    measurements = DataFrame(obs_id=["obs_a", "obs_a"],
+                             time=[0, 10.0],
+                             measurement=[0.7, 0.1],
+                             noise_parameters=0.5)
+    petab_parameters = [PEtabParameter(:a0, value=1.0, scale=:lin, prior=Normal(1.5, 0.1)),
+                        PEtabParameter(:b0, value=0.0, scale=:lin, prior=Normal(1.5, 0.001)),
+                        PEtabParameter(:k1, value=0.8, scale=:lin),
+                        PEtabParameter(:k2, value=0.6, scale=:lin)]
+    @unpack A = rn
+    observables = Dict("obs_a" => PEtabObservable(A, 0.5))
+    petab_model = PEtabModel(rn, observables, measurements,
+                             petab_parameters, verbose=false)
+    petab_problem = PEtabODEProblem(petab_model, verbose=false)
+    startguesses = generate_startguesses(petab_problem, 10000; verbose=false)
+    @test mean(startguesses[1, :]) ≈ 1.5 atol=1e-1
+    @test std(startguesses[1, :]) ≈ 0.1 atol=1e-1
+    startguesses = generate_startguesses(petab_problem, 1; verbose=false)
+    @test startguesses isa Vector{Float64}
+    @test startguesses[2] ≤ 1.6 && startguesses[2] ≥ 1.4
 
     # Test optimisers
     path_yaml = joinpath(@__DIR__, "Test_model2", "Test_model2.yaml")
