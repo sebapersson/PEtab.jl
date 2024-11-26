f696db1b06b706be4dc79bfb3e4c10a5ecde44e4
sebapersson <sebastian.persson1@gmail.com>
2024-11-26 16:45:07 +0100

    Update PEtab SciML import tests

--- test/petab_sciml/petab_sciml_testsuite.jl
+++ test/petab_sciml/petab_sciml_testsuite.jl
@@ -4,10 +4,10 @@ include(joinpath(@__DIR__, "helper.jl"))
 # TODO: Refactor transform to be more clear what is transformed (mech or nn)
 # 9min
 @testset "PEtab SciML extension" begin
-    for i in 1:7
+    for i in 1:16
         test_case = i < 10 ? "00$i" : "0$i"
         path_yaml = joinpath(@__DIR__, "test_cases", test_case, "petab", "problem_ude.yaml")
-        nnmodels = PEtab.load_nets(path_yaml)
+        nnmodels = PEtab.load_nnmodels(path_yaml)
         osolver = ODESolver(Rodas5P(autodiff = false), abstol = 1e-10, reltol = 1e-10)
         model = PEtabModel(path_yaml; nnmodels = nnmodels, verbose = false)
         for config in PROB_CONFIGS
@@ -25,3 +25,8 @@ include(joinpath(@__DIR__, "helper.jl"))
         end
     end
 end
+
+petab_prob = PEtabODEProblem(model; odesolver = osolver, split_over_conditions = true, gradient_method = :ForwardDiff)
+x = get_x(petab_prob)
+petab_prob.nllh(x)
+petab_prob.grad(x)
