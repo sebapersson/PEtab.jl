daa02c4cf216014c06ba329aaa2c0b8042cff85b
sebapersson <sebastian.persson1@gmail.com>
2025-03-08 12:49:04 +0000

    Update test tolerances following OrdinaryDiffEq v6.92

--- test/published_models.jl
+++ test/published_models.jl
@@ -3,8 +3,8 @@
     published models. Also check gradients for selected models using FiniteDifferences package
 =#
 
-using PEtab, OrdinaryDiffEq, SciMLSensitivity, ForwardDiff, LinearAlgebra, FiniteDifferences,
-      Sundials, Test
+using PEtab, OrdinaryDiffEqRosenbrock, SciMLSensitivity, LinearAlgebra, FiniteDifferences,
+    Sundials, Test
 
 include(joinpath(@__DIR__, "common.jl"))
 
@@ -26,55 +26,56 @@ GRAD_MODELS = Dict(:Bachmann_MSB2011 => (test=[:forward_AD, :forward_eqs, :forwa
                    :Brannmark_JBC2010 => (test=[:forward_AD, :forward_eqs], tol=2e-3, odetol = 1e-10, split = false),
                    :Schwen_PONE2014 => (test=[:forward_AD, :forward_eqs], tol=1e-3, odetol = 1e-8, split = false))
 
-function test_nllh(model::Symbol)::Nothing
-    @info "nllh model $model"
-    path = joinpath(@__DIR__, "published_models", "$model", "$(model).yaml")
-    petab_model = PEtabModel(path; verbose = false, build_julia_files = true,
-                             write_to_file = false)
-    osolver = ODESolver(Rodas4P(), abstol = 1e-12, reltol = 1e-12, maxiters = Int(1e5))
-    prob = PEtabODEProblem(petab_model; odesolver = osolver, verbose = false)
-    nllh = prob.nllh(prob.xnominal_transformed)
-    nllh_ref = NLLH_MODELS[model].nllh
-    prior_ref = NLLH_MODELS[model].prior
+function test_nllh(modelid::Symbol)::Nothing
+    @info "nllh model $modelid"
+    path = joinpath(@__DIR__, "published_models", "$modelid", "$(modelid).yaml")
+    model = PEtabModel(path)
+    osolver = ODESolver(Rodas4P(), abstol = 1e-11, reltol = 1e-11, maxiters = Int(1e5))
+    ssolver = SteadyStateSolver(:Simulate; abstol=5e-10, reltol=1e-12, maxiters = Int(1e5))
+    prob = PEtabODEProblem(model; odesolver = osolver, ss_solver = ssolver,
+                           sparse_jacobian = false)
+    nllh = prob.nllh(get_x(prob))
+    nllh_ref = NLLH_MODELS[modelid].nllh
+    prior_ref = NLLH_MODELS[modelid].prior
     @test nllh ≈ nllh_ref + prior_ref atol = 1e-2
     return nothing
 end
 
-function test_grad(model::Symbol)::Nothing
-    @info "Gradient model $model"
-    path = joinpath(@__DIR__, "published_models", "$model", "$(model).yaml")
-    petab_model = PEtabModel(path; verbose = false, build_julia_files = true,
-                             write_to_file = false)
+function test_grad(modelid::Symbol)::Nothing
+    @info "Gradient model $modelid"
+    path = joinpath(@__DIR__, "published_models", "$modelid", "$(modelid).yaml")
+    model = PEtabModel(path)
 
-    odetol = GRAD_MODELS[model].odetol
-    grads_test = GRAD_MODELS[model].test
-    testtol = GRAD_MODELS[model].tol
-    split = GRAD_MODELS[model].split
+    odetol = GRAD_MODELS[modelid].odetol
+    grads_test = GRAD_MODELS[modelid].test
+    testtol = GRAD_MODELS[modelid].tol
+    split = GRAD_MODELS[modelid].split
 
-    ss_solver = SteadyStateSolver(:Simulate; abstol=1e-12, reltol=1e-10)
-    osolver1 = ODESolver(Rodas5P(), abstol = odetol, reltol = odetol)
+    ss_solver = SteadyStateSolver(:Simulate; abstol=5e-10, reltol=1e-12, maxiters = Int(1e5))
+    osolver1 = ODESolver(Rodas5P(), abstol = odetol, reltol = odetol, maxiters = Int(1e5))
     osolver2 = ODESolver(CVODE_BDF(), abstol = odetol, reltol = odetol)
-    prob_ref = PEtabODEProblem(petab_model; odesolver = osolver1, ss_solver = ss_solver,
+    prob_ref = PEtabODEProblem(model; odesolver = osolver1, ss_solver = ss_solver,
                                verbose = false)
-    x = prob_ref.xnominal_transformed
+    x = get_x(prob_ref)
+    prob_ref.nllh(x)
     gref = FiniteDifferences.grad(central_fdm(5, 1), prob_ref.nllh, x)[1]
 
     for grad_test in grads_test
         @info "Method $(grad_test)"
         if grad_test == :forward_eqs_sciml
-            g = _compute_grad(x, petab_model, :ForwardEquations, osolver2;
+            g = _compute_grad(x, model, :ForwardEquations, osolver2;
                               ss_solver = ss_solver, sensealg = ForwardSensitivity())
         elseif grad_test == :forward_eqs
-            g = _compute_grad(x, petab_model, :ForwardEquations, osolver1;
+            g = _compute_grad(x, model, :ForwardEquations, osolver1;
                               ss_solver = ss_solver, split = split)
         elseif grad_test == :adjoint
-            g = _compute_grad(x, petab_model, :Adjoint, osolver1;  ss_solver = ss_solver,
+            g = _compute_grad(x, model, :Adjoint, osolver1;  ss_solver = ss_solver,
                               sensealg = InterpolatingAdjoint(autojacvec=ReverseDiffVJP(true)))
         else
-            g = _compute_grad(x, petab_model, :ForwardDiff, osolver1; ss_solver = ss_solver,
+            g = _compute_grad(x, model, :ForwardDiff, osolver1; ss_solver = ss_solver,
                               split = split)
         end
-        @test norm(gref - g) ≤ testtol
+        @test all(.≈(gref, g; atol = testtol))
     end
     return nothing
 end
