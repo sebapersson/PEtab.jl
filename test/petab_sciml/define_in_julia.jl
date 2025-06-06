ed8740763934cb2c06c6b6006a890f436e975a9d
sebapersson <sebastian.persson1@gmail.com>
2025-06-06 13:08:39 +0100

    Update compat

--- test/petab_sciml/define_in_julia.jl
+++ test/petab_sciml/define_in_julia.jl
@@ -3,7 +3,7 @@ include(joinpath(@__DIR__, "helper.jl"))
 @testset "SciML model in Julia" begin
     for i in 1:28
         i in [14, 15] && continue
-        test_case = i > 10 ? "0$(i).jl" : "00$(i).jl"
+        test_case = i ≥ 10 ? "0$(i).jl" : "00$(i).jl"
         include(joinpath(@__DIR__, test_case))
     end
 end
