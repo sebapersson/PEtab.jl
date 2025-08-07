2f8de38b72e68133c1d66cf1e386f81a8a8b0cf3
sebapersson <sebastian.persson1@gmail.com>
2025-08-07 09:14:33 +0100

    Change to using Fides for timing test for effiency

--- test/calibrate.jl
+++ test/calibrate.jl
@@ -1,5 +1,4 @@
-using PEtab, Distributions, CSV, DataFrames, OrdinaryDiffEq, Catalyst, ComponentArrays,
-      Optim, Ipopt, Optimization, OptimizationOptimJL, PyCall, Test
+using Ipopt, Optim, PEtab, PyCall, Test
 
 @testset "Calibrate single start Fides" begin
     path_yaml = joinpath(@__DIR__, "analytic_solution", "Test_model2.yaml")
@@ -30,17 +29,17 @@ end
     dirsave = joinpath(@__DIR__, "calibrate_tmp")
     res1 = calibrate_multistart(prob, Optim.IPNewton(), 10; save_trace=true,
                                 dirsave = dirsave, nprocs = 2)
-    #res2 = calibrate_multistart(prob, IpoptOptimizer(true), 10; nprocs = 2)
-    #res3 = calibrate_multistart(prob, Fides(:BFGS), 10; nprocs = 2)
+    res2 = calibrate_multistart(prob, IpoptOptimizer(true), 10; nprocs = 2)
+    res3 = calibrate_multistart(prob, Fides(:BFGS), 10; nprocs = 2)
     res_read = PEtabMultistartResult(dirsave)
     @test all(.≈(res1.xmin, get_x(prob), atol = 1e-2))
-    #@test all(.≈(res2.xmin, get_x(prob), atol = 1e-2))
-    #@test all(.≈(res3.xmin, get_x(prob), atol = 1e-2))
-    #@test all(.≈(res_read.xmin, get_x(prob), atol = 1e-2))
+    @test all(.≈(res2.xmin, get_x(prob), atol = 1e-2))
+    @test all(.≈(res3.xmin, get_x(prob), atol = 1e-2))
+    @test all(.≈(res_read.xmin, get_x(prob), atol = 1e-2))
     # Due to startup overhead many multistarts must be performed two find the effect
     # in runtime
-    #b1 = @elapsed res1 = calibrate_multistart(prob, Optim.IPNewton(), 2500; nprocs = 1)
-    #b2 = @elapsed res2 = calibrate_multistart(prob, Optim.IPNewton(), 2500; nprocs = 2)
-    #@test b1 > b2
+    b1 = @elapsed res1 = calibrate_multistart(prob, Fides(:BFGS), 2500; nprocs = 1)
+    b2 = @elapsed res2 = calibrate_multistart(prob, Fides(:BFGS), 2500; nprocs = 2)
+    @test b1 > b2
     rm(dirsave; recursive = true)
 end
