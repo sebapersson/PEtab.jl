760ad7b3aaae988ddeb69f1d79b325032631cfb7
sebapersson <sebastian.persson1@gmail.com>
2026-03-05 09:47:48 +0000

    Add export support for SciML models

--- test/export.jl
+++ test/export.jl
@@ -1,4 +1,4 @@
-using PEtab, Test
+using Lux, PEtab, Test
 
 # v1 problem
 path_yaml = joinpath(
@@ -24,6 +24,23 @@ prob_export = PEtabModel(path_yaml) |>
 x_export = get_x(prob_export) .* 0.9
 export_petab(dir_export, prob_export, x_export)
 prob_exported = PEtabModel(joinpath(dir_export, basename(path_yaml))) |>
+    PEtabODEProblem
+@test get_x(prob_exported) == x_export
+rm(dir_export; recursive = true)
+
+# PEtab-SciML problem
+path_yaml = joinpath(
+    @__DIR__, "petab_sciml_testsuite", "test_cases", "sciml_problem_import", "001",
+    "petab", "problem.yaml"
+)
+dir_export = joinpath(@__DIR__, "petpetab_sciml_testsuiteb_v2_testsuite", "export_tmp")
+ml_models = MLModels(path_yaml)
+prob_export = PEtabModel(path_yaml; ml_models = ml_models) |>
+    PEtabODEProblem
+x_export = get_x(prob_export) .* 0.9
+export_petab(dir_export, prob_export, x_export)
+path_exported = joinpath(dir_export, basename(path_yaml))
+prob_exported = PEtabModel(path_exported; ml_models = MLModels(path_exported)) |>
     PEtabODEProblem
 @test get_x(prob_exported) == x_export
 rm(dir_export; recursive = true)
