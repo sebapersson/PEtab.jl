dc8d6e83d855e6c6372fb9ff27c1cd6becfa58c4
sebapersson <sebastian.persson1@gmail.com>
2026-03-09 09:13:38 +0000

    Use invokelatest to avoid the world problem

--- test/petab_sciml_testsuite.jl
+++ test/petab_sciml_testsuite.jl
@@ -1,7 +1,6 @@
 include(joinpath(@__DIR__, "petab_sciml_testsuite", "helper.jl"))
 
 dir_tests = joinpath(@__DIR__, "petab_sciml_testsuite", "test_cases")
-
 # Global needed to avoid world-problem on Julia 1.12
 global lux_model
 @testset "ML import" begin
@@ -17,43 +16,39 @@ global lux_model
     end
 end
 
-global ml_models
 ode_solver = ODESolver(
     Rodas5P(autodiff = false), abstol = 1.0e-10, reltol = 1.0e-10, maxiters = Int(1.0e6)
 )
-# Somehow, this resolves the world-problem on Julia 1.12
-path_yaml = joinpath(dir_tests, "sciml_problem_import", "001", "petab", "problem.yaml")
-ml_models = MLModels(path_yaml)
-model = PEtabModel(path_yaml; ml_models = ml_models)
-prob = PEtabODEProblem(model; odesolver = ode_solver)
-prob.nllh(get_x(prob))
-prob.grad(get_x(prob))
-for i in 1:39
-    global ml_models
-    test_case = i < 10 ? "00$i" : "0$i"
-    path_yaml = joinpath(
-        dir_tests, "sciml_problem_import", test_case, "petab", "problem.yaml"
-    )
-    ml_models = MLModels(path_yaml)
-    model = PEtabModel(path_yaml; ml_models = ml_models)
-    for config in PROB_CONFIGS
-        # Edge case for underperforming configuration. So even though support could be
-        # added in theory, it is not priority
-        if (
-                config.grad == :ForwardEquations && config.split == false &&
-                    config.sensealg == :ForwardDiff
-            )
-            continue
-        end
-        # To long time, will not be used in practice
-        if config.sensealg == ForwardSensitivity()
-            continue
-        end
-        petab_prob = PEtabODEProblem(
-            model; odesolver = ode_solver, gradient_method = config.grad,
-            split_over_conditions = config.split, sensealg = config.sensealg
+@testset "PEtab SciML problem import" begin
+    for i in 1:39
+        test_case = i < 10 ? "00$i" : "0$i"
+        path_yaml = joinpath(
+            dir_tests, "sciml_problem_import", test_case, "petab", "problem.yaml"
         )
-        test_hybrid(test_case, petab_prob)
+        ml_models = MLModels(path_yaml)
+        model = PEtabModel(path_yaml; ml_models = ml_models)
+        for config in PROB_CONFIGS
+            # Edge case for underperforming configuration. So even though support could be
+            # added in theory, it is not priority
+            if (
+                    config.grad == :ForwardEquations && config.split == false &&
+                        config.sensealg == :ForwardDiff
+                )
+                continue
+            end
+            # To long time, will not be used in practice
+            if config.sensealg == ForwardSensitivity()
+                continue
+            end
+            # invoke latest to avoid world-problem on Julia 1.12
+            petab_prob = Base.invokelatest(
+                model -> PEtabODEProblem(
+                    model; odesolver = ode_solver, gradient_method = config.grad,
+                    split_over_conditions = config.split, sensealg = config.sensealg
+                ), model
+            )
+            Base.invokelatest(test_hybrid, test_case, petab_prob)
+        end
     end
 end
 
@@ -71,7 +66,9 @@ end
 end
 
 # PEtab SciML Julia interface
-ode_solver = ODESolver(Rodas5P(), abstol = 1.0e-10, reltol = 1.0e-10, maxiters = Int(1.0e6))
+ode_solver = ODESolver(
+    Rodas5P(autodiff = false), abstol = 1.0e-10, reltol = 1.0e-10, maxiters = Int(1.0e6)
+)
 @testset "SciML model in Julia" begin
     for i in 1:39
         # Test-cases with no-equivalent support in the Julia interface
